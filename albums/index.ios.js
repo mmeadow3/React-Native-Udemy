8ab0a8542b52854833d9511da230bd46de99bd58
Mike Meadows <mmeadow33@gmail.com>
2017-09-21 13:57:16 -0500

    initial commit

---
+++ albums/index.ios.js
@@ -0,0 +1,7 @@
+// import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+// create a component
+const App = () =>  (<Text>Some Text</Text>);
+////render the component to the device
+AppRegistry.registerComponent('albums', () => App);
