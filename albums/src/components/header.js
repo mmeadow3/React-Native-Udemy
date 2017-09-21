45059fecc856630605ba0f94cff161759c35b6f4
Mike Meadows <mmeadow33@gmail.com>
2017-09-21 14:59:06 -0500

    added view and styles

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,7 +1,25 @@
 // import a library to help create a component
 import React from 'react';
-import { Text } from 'react-native';
+// import { Text } from 'react-native';
+import { Text, View } from 'react-native';//////view is used to get the view deminsions
 // create a component
-const Header = () => (<Text>Albums</Text>);
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+    <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle:{
+    backgroundColor: 'red'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
 ////make component available to other parts of the App
 export default Header;
