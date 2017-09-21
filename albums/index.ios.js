f03a2a7964fec1c1307df32ab0142bfbded557ff
Mike Meadows <mmeadow33@gmail.com>
2017-09-21 14:42:15 -0500

    adding header

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,7 +1,9 @@
 // import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+// import { Text, AppRegistry } from 'react-native';      /////// Removed Text from import
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 // create a component
-const App = () =>  (<Text>Some Text</Text>);
+const App = () => (<Header />);
 ////render the component to the device
 AppRegistry.registerComponent('albums', () => App);
---
+++ albums/src/components/header.js
@@ -0,0 +1,7 @@
+// import a library to help create a component
+import React from 'react';
+import { Text } from 'react-native';
+// create a component
+const Header = () => (<Text>Albums</Text>);
+////make component available to other parts of the App
+export default Header;
