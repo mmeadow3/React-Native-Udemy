19b415b2db7622997996224b1e1d83afd8b962f3
Mike Meadows <mmeadow33@gmail.com>
2017-09-22 09:14:12 -0500

    rendering the data from class function

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Axios from 'axios';
 
 ////////////////////This is a class functional component
 ///so we need to make class components for better functionality
@@ -11,15 +12,25 @@ import { View, Text } from 'react-native';
 //   );
 // };
 class AlbumList extends Component {
+  state = { albums: [] };//////////initial delcaration at start of load
+
   componentWillMount() {
     //////cmd + d = bring up debugger
     console.log('mounting');
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    // .then(response => console.log(response));
+    .then(response => this.setState({ albums: response.data }));//////set state will fire when (this.state) changes
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{ album.title }</Text>);
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>List</Text>
+        { this.renderAlbums() }
       </View>
     );
   }
